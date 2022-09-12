4936a18631cf7e692f6c23419efa86a52b707610
Connor-Berkman <bcr346140@gmail.com>
2022-09-12 11:49:11 -0300

    Initial commit

---
+++ test3HelloWorld2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helllo World!");
+            Console.WriteLine("");
+            Console.WriteLine("This took way to many tries to get this to work.");
+            Console.ReadKey(true);
+        }
+    }
+}
